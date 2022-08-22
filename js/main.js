2f89cc12a9b0d63f96522957982c8b5703a03630
HasanMt12 <hasan.2mahmud22gmail.com>
2022-08-23 05:06:10 +0600

    js file problem solved

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // Common js Function
-
-function getElementById(getElementId, isTrue) {
+function getElementById(getElementId, isTrue){
     let elementId = document.getElementById(getElementId);
     let elementContent;
 
-    if (isTrue === true) {
+    if(isTrue === true){
         elementContent = parseInt(elementId.value);
-    } else {
+    }
+    else{
         elementContent == parseInt(elementId.innerText)
     }
     elementId = elementContent;
@@ -22,57 +22,55 @@ function playerAdded() {
         let addSelectBtn = playerNameList[i].selectBtn;
 
 
-        if (i != 5) {
-            let playerList = document.createElement("ul");
-            playerList.innerHTML = `
+    if (i != 5) {
+        let playerList = document.createElement("ul");
+        playerList.innerHTML = `
         <li>${i + 1}. ${addPlayerName}</li>
         
             
     `;
-            // Button desabled
-            playerAddList.appendChild(playerList);
-            addSelectBtn.setAttribute("disabled", true)
+        // Button desabled
+        playerAddList.appendChild(playerList);
+        addSelectBtn.setAttribute("disabled", true)
+    document.getElementById("per-player-clculate-btn").addEventListener("click", function(){
+        let perPlayer = getElementById("per-player", true);
+        let clculatePerPlayerPrice = perPlayer *  i;
+         // display player expenses
+        let playerExpenses = document.getElementById("player-expenses");
+        let playerExpensesPrice = parseInt(playerExpenses.innerText);
+        playerExpenses.innerText = clculatePerPlayerPrice.toFixed(2);
+        // Calculate Total
+    document.getElementById("calculate-total").addEventListener("click", function(){
 
-            document.getElementById("per-player-clculate-btn").addEventListener("click", function () {
-                let perPlayer = getElementById("per-player", true);
-                let clculatePerPlayerPrice = perPlayer * i;
-                // display player expenses
-                let playerExpenses = document.getElementById("player-expenses");
-                let playerExpensesPrice = parseInt(playerExpenses.innerText);
-                playerExpenses.innerText = clculatePerPlayerPrice.toFixed(2);
+                // Manager
+        let manager = getElementById("manager", true)
+                // Coach
+        coach = getElementById("manager", true)
 
-                // Calculate Total
-                document.getElementById("calculate-total").addEventListener("click", function () {
-
-                    // Manager
-                    let manager = getElementById("manager", true)
-                    // Coach
-                    coach = getElementById("manager", true)
-
-                    let managerCoachTotal = manager + coach
-                    // Total expenses
-                    let totalExpenses = playerExpensesPrice + managerCoachTotal;
-                    // display total price
-                    let total = document.getElementById("total");
-                    total.innerText = totalExpenses.toFixed(2)
-                })
+        let managerCoachTotal = manager + coach  
+                // Total expenses
+            let totalExpenses = playerExpensesPrice + managerCoachTotal;
+                // display total price
+            let total = document.getElementById("total");
+                    total.innerText = totalExpenses.toFixed(2) 
+                })    
             })
-        } else {
+        } 
+        else {
             alert("You Can't Add More Than 5 Players...");
             return
         }
     }
 }
 
-// select player name and collect and added/ push 
+        // select player name and collect and added/ push 
 let playerNameList = [];
-
 function selectPlayers(element) {
     let playerName = element.parentNode.children[0].innerText;
     let selectBtn = element.parentNode.children[2];
     let playerObj = {
         playerName: playerName,
-        selectBtn: selectBtn
+        selectBtn : selectBtn
     }
     playerNameList.push(playerObj);
     let setPlayerList = playerAdded(playerNameList);
